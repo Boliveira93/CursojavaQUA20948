0a1efdafab288851480e15403ed6d14d80745820
Boliveira93 <bruno.silvaoliveira.s@gmail.com>
2022-07-22 00:30:16 -0300

    Meu primeiro commit

---
+++ PrimeiroPrograma Java/src/aulas/HelloWorld.java
@@ -0,0 +1,23 @@
+package aulas;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero1 = 10;
+		int numero2 = 20;
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+		int idade = 29;
+		String nome = "Bruno";
+		double dinheiro = 5.25;
+
+		System.out.println(nome + " tem " + idade + " anos, e tem " + dinheiro + "reais");
+
+		
+
+	}
+
+}
